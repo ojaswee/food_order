eeb4e383f06b2a68c034899827e0ce13379162bd
Ojaswee Dhungana <ojaswee@outlook.com>
2025-06-06 14:39:25 -0700

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import logo from '../assets/logo.jpg';
+
+export default function Header() {
+	return (
+		<header id="main-header">
+			<div id ="title">
+				<img src={logo} alt="Logo" />
+				<h1>Order Delicious Food</h1>
+			</div>
+			<nav>
+				<button className="btn">Cart(0)</button>
+			</nav>
+		</header>
+	);
+}
